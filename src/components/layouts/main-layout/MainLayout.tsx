b321c06a4e9194bf8b8ef4ee7b36bf44161bbe59
Aleksandr Salnikov <taynbiy@gmail.com>
2025-10-16 06:10:02 +0500

    change layout size

--- src/components/layouts/main-layout/MainLayout.tsx
+++ src/components/layouts/main-layout/MainLayout.tsx
@@ -1,12 +1,12 @@
-import { ThemeModal } from '@/components/ui/modals/ThemeModal'
 import { PropsWithChildren } from 'react'
-import { Footer } from './footer/Footer'
 import { Header } from './header/Header'
+import { Footer } from './footer/Footer'
+import { ThemeModal } from '@/components/ui/modals/ThemeModal'
 
 export function MainLayout({ children }: PropsWithChildren<unknown>) {
 	return (
-		<div className='flex min-h-screen'>
-			<div className='flex-1'>
+		<div className='min-h-screen'>
+			<div className='flex flex-col flex-1'>
 				<Header />
 				<main className='relative h-full'>{children}</main>
 				<ThemeModal />
