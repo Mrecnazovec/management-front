e5526c27dc4195122d6b78114ddeeac0045f7bb9
Aleksandr Salnikov <taynbiy@gmail.com>
2025-12-07 16:19:04 +0500

    test access token

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,20 +3,20 @@ import { EnumTokens } from './services/auth/auth-token.service'
 import { ADMIN_URL } from './config/url.config'
 
 export async function middleware(request: NextRequest) {
-	const refreshToken = request.cookies.get(EnumTokens.REFRESH_TOKEN)?.value
+	const accessToken = request.cookies.get(EnumTokens.ACCESS_TOKEN)?.value
 
 	const isAuthPage = request.url.includes(ADMIN_URL.auth())
 
 	if (isAuthPage) {
-		if (refreshToken) {
+		if (accessToken) {
 			return NextResponse.redirect(new URL(ADMIN_URL.home(), request.url))
 		}
 
 		return NextResponse.next()
 	}
 
-	if (refreshToken === undefined) {
-		console.log(`refreshToken: ${refreshToken}`)
+	if (accessToken === undefined) {
+		console.log(`accessToken: ${accessToken}`)
 
 		return NextResponse.redirect(new URL(ADMIN_URL.auth(), request.url))
 	}
