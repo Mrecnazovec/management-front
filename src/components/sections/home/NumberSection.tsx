5d7f00d0a2bc0074271bb12e82fce9c3be207284
Aleksandr Salnikov <taynbiy@gmail.com>
2025-12-09 10:31:35 +0500

    change number section

--- src/components/sections/home/NumberSection.tsx
+++ src/components/sections/home/NumberSection.tsx
@@ -4,7 +4,7 @@ import CountUp from 'react-countup'
 export function NumberSection() {
 	return (
 		<Container className=''>
-			<h2 className='text-center text-4xl mb-15'>Менеджмент в цифрах</h2>
+			<h2 className='text-center text-4xl mb-15'>Менеджмент в числах</h2>
 			<div className='flex justify-around flex-row max-[470px]:flex-col items-center gap-4 flex-wrap mb-25'>
 				<div className='w-fit flex flex-col items-center justify-center'>
 					<div className='flex items-center justify-center rounded-full md:size-[180px] size-[130px] bg-gradient-to-b from-main to-secondary mb-2'>
@@ -26,7 +26,8 @@ export function NumberSection() {
 				<div className='w-fit flex flex-col items-center justify-center'>
 					<div className='flex items-center justify-center rounded-full md:size-[180px] size-[130px] bg-gradient-to-b from-main to-secondary mb-2'>
 						<div className='flex items-center justify-center rounded-full size-[90%] bg-white'>
-							<CountUp end={85} className='text-3xl'></CountUp>
+							<span className='text-3xl'>{'>'}</span>
+							<CountUp end={130} className='text-3xl'></CountUp>
 						</div>
 					</div>
 					<p className='text-center text-2xl'>Студентов</p>
